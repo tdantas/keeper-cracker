3a9313529b6c67b271e408d29d160f94b61d3170
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2020-02-09 10:42:39 +0000

    format code

--- src/fexco/main.clj
+++ src/fexco/main.clj
@@ -3,6 +3,7 @@
   (:require [clojure.java.io :as io]))
 
 ;; - cpu intensive task
+
 (defn- try-fn [plain encrypted]
   (Thread/sleep 50)
   (> (rand-int 1000) 998))
@@ -16,13 +17,14 @@
 
 (defn run [filename encrypted-password]
   (with-open [rdr (io/reader (io/file filename))]
-    (->>(partition-all 100 (line-seq rdr))
-        (pmap #(crack encrypted-password %))
-        (reduce #(when (seq %2) (reduced %2)) {}))))
+    (->> (partition-all 100 (line-seq rdr))
+         (pmap #(crack encrypted-password %))
+         (reduce #(when (seq %2) (reduced %2)) {}))))
 
 
 ;; we could validate the filename / encrypted password
 ;; but for simplicify I'm assuming the inputs are correct
+
 (defn -main [& args]
   (let [[filename encrypted-password] args]
     (println "Password: " (run filename encrypted-password))))
